531f085225c45b12d718b7994b5fe389d6b0ba6c
Thomas Lefebvre <t.lefebvre53@gmail.com>
2026-04-30 09:30:29 +0200

    Feat(profile) : adding avatar and log out button

--- frontend/src/components/page/Header.tsx
+++ frontend/src/components/page/Header.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+import {Button} from "../ui/button";
 
 export default function Header() {
     return (
         <header className="bg-blue-400 text-white p-4">
-        <h1 className="text-2xl font-bold text-center">Hypertube</h1>
+            <div className="flex items-center justify-between">
+                <Avatar className="size-20 ml-8 bg-gray-300">
+                    <AvatarImage
+                        src={`https://robohash.org/1.png?set=set1`}
+                        alt="Avatar"
+                    />
+                    <AvatarFallback>HT</AvatarFallback>
+                </Avatar>
+                <h1 className="text-2xl text-gray-800 font-bold text-center flex-1">Hypertube</h1>
+                <Button className="mr-8 bg-red-500 hover:bg-red-600 text-white">
+                    Se déconnecter
+                </Button>
+            </div>            
         </header>
     );
-    }
+}
